3b3fce18c224485227ece4ead982f7403bd26c0f
Brian Oliver II <brian.d.oliver@icloud.com>
2021-04-24 10:32:17 -0400

    updated signup.js to support image upload on signup

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+import {Form, Input, Button} from "reactstrap";
+
+export default function Profile({token}) {
+    
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -11,24 +11,49 @@ export default function SignUp({updateToken}) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
+    const [profileImage, setProfileImage] = useState();
       
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
-
+        /*
+            This will handle two things. End result, we have newly created user
+            with a profileImage attached to there profile.
+            1 - create the user in the database
+            2 - upload image file
+        */
         try {
-            fetch(`${process.env.REACT_APP_API_SERVER_BASE_URL}/user/signup`, {
+            // create the user
+            const createdUser = await fetch(`${process.env.REACT_APP_API_SERVER_BASE_URL}/user/signup`, {
                 method: "POST",
                 body: JSON.stringify({
                     user:{firstName, lastName, username, email, password, confirmPassword}
                 }),
                 headers: new Headers({"Content-Type":"application/json"})
-            }).then(res => res.json()).then(data => {
-                updateToken(data.sessionToken);
-                history.push("/user/profile");
-            });
+            }).then(res => res.json())
+            // updateToken
+            updateToken(createdUser.sessionToken);
+            // if we do not have an image, redirect to user profile
+            if (!profileImage) return history.push("/profile");
+            // proccess image upload
+            const formData = new FormData();
+            formData.append("image", profileImage);
+            // upload image
+            const uploadResult = await fetch(`${process.env.REACT_APP_API_SERVER_BASE_URL}/upload?type=user`, {
+                method: "POST",
+                body: formData,
+                headers: new Headers({
+                    "Authorization": `Bearer ${createdUser.sessionToken}`
+                })
+            }).then(res => res.json());
+            // redirect to profile
+            history.push("/profile");
         } catch (err) {
             console.log(err);
         }
+    };
+
+    const fileUploadHandler = (event) => {
+        setProfileImage(event.target.files[0]);
     };
 
     return (
@@ -75,6 +100,15 @@ export default function SignUp({updateToken}) {
                         <FormGroup>
                             <Label htmlFor="signup-confirm-password">Confirm Password:</Label>
                             <Input required type="password" name="signup-confirm-password" id="signup-confirm-password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)}/>
+                        </FormGroup>
+                    </Col>
+                </Row>
+
+                <Row className="mt-2">
+                    <Col>
+                        <FormGroup>
+                            <Label htmlFor="profile-image-upload" className="me-2">Upload a Profile Photo:</Label>
+                            <Input type="file" name="file" id="profile-image-upload" onChange={fileUploadHandler}/>
                         </FormGroup>
                     </Col>
                 </Row>

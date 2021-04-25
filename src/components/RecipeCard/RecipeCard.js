cdf14eb8866f8b088992d4d443823cb64648b3bc
Brian Oliver II <brian.d.oliver@icloud.com>
2021-04-25 16:49:21 -0400

    fixed modal transactions for recipe cards

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -1,24 +1,29 @@
-import React from "react";
-import { Card, CardImg, CardBody, CardTitle, CardText, Col } from "reactstrap";
+import React, {useState, useContext} from "react";
+import { Card, CardImg, CardBody, CardTitle, CardText, Col} from "reactstrap";
 import genericRecipeImage from "../../assets/generic_recipe_img.svg";
+import UserRecipeModal from "./UserRecipeModal";
+import PublicRecipeModal from "./PublicRecipeModal";
+import UserContext from "../../context/UserContext";
+import { useParams } from "react-router-dom";
 
 export default function Recipe({recipe}) {
-
-    const cardClicked = () => {
-        console.log("card clicked")
-    }
-
+    const {username} = useParams();
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const toggle = () => setModalIsOpen(!modalIsOpen);
+    const userContext = useContext(UserContext);
 
     return (
-        <Col sm={12} md={6} lg={4} xl={3}>
-            <Card className="recipe-card mb-3" onClick={cardClicked}>
+        <Col sm={12} md={6} lg={4} xl={3}>            
+            <Card className="recipe-card mb-3" onClick={toggle}>
                 <CardImg className="recipe.card-image" src={!recipe.recipeImageURL ? genericRecipeImage : recipe.recipeImageURL} alt="recipe image" />
-                {}
                 <CardBody className="recipe-card-body">
                     <CardTitle tag="h5">{recipe.recipeName}</CardTitle>
                     <CardText>{recipe.description}</CardText>
                 </CardBody>
             </Card>
+            {/* If user isAuth, show UserRecipeModal else PublicRecipeModal */}
+            {userContext.isAuth && userContext.user.username === username ? <UserRecipeModal isOpen={modalIsOpen} toggle={toggle} recipe={recipe} /> :
+                                    <PublicRecipeModal isOpen={modalIsOpen} toggle={toggle} recipe={recipe}/>}
         </Col>
     )
 }
